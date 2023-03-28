3f93887e4fa0090f790a0548315c6387dd038edb
Andrey Litvinov <andreoyy.litvinovv@gmail.com>
2023-03-28 10:21:43 +0300

    Green theme added

--- app/src/main/java/com/example/trainercard/MainActivity.kt
+++ app/src/main/java/com/example/trainercard/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // this.setTheme(R.style.AppTheme_Overlay)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
