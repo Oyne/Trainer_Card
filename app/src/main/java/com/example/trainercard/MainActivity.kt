d742f9f52a2e4b971e37369e1c4e5c552b0a6883
Andrey Litvinov <andreoyy.litvinovv@gmail.com>
2023-03-29 15:33:44 +0300

    Themes added

--- app/src/main/java/com/example/trainercard/MainActivity.kt
+++ app/src/main/java/com/example/trainercard/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.trainercard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint.Style
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        this.setTheme(R.style.AppTheme_Overlay)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
